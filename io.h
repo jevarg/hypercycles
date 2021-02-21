bee4cca3097755d3d8c9b3e5a9876099e3f025b3
Jean <jean.gravier@pm.me>
2021-02-21 20:16:37 +0100

    Removed "far" DOS keyword

--- io.h
+++ io.h
@@ -1,21 +1,22 @@
 
-const JOY_X=1, JOY_Y=2;
-const JBUTTON1=0x10, JBUTTON2=0x20;
-const LMBUTTON=1, RMBUTTON=2;
+const JOY_X = 1, JOY_Y = 2;
+const JBUTTON1 = 0x10, JBUTTON2 = 0x20;
+const LMBUTTON = 1, RMBUTTON = 2;
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
-	int readstick(int);				// Read joystick position
-	int readjbutton(int);			// Read joystick button
-	int	initmouse();					// Initialize mouse driver
-	void rempointer();				// Remove mouse pointer from display
-  	void disppointer();       // Show mouse pointer on display
-	int readmbutton();				// Read mouse buttons
-	void relpos(int *,int *);	// Get relative mouse position
-	int	initkey(char far *buffer); // Initialize keyboard handler
-  	int getscan();            // Get last scan code
-  	int remkey();             // Remove keyboard handler
+	int readstick(int);					 // Read joystick position
+	int readjbutton(int);				 // Read joystick button
+	int initmouse();					 // Initialize mouse driver
+	void rempointer();					 // Remove mouse pointer from display
+	void disppointer();					 // Show mouse pointer on display
+	int readmbutton();					 // Read mouse buttons
+	void relpos(int *, int *);			 // Get relative mouse position
+	int initkey(char /* far */ *buffer); // Initialize keyboard handler
+	int getscan();						 // Get last scan code
+	int remkey();						 // Remove keyboard handler
 #ifdef __cplusplus
 }
 #endif
