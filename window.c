8856d9e8619558a6b5a3d75d8b864c68846274bf
jevarg <jevarg@pm.me>
2021-03-12 23:28:01 +0100

    Set sdl draw color to black.

--- window.c
+++ window.c
@@ -31,21 +31,27 @@ initialize_ui()
     ui.window = SDL_CreateWindow("Hypercycles", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, 0);
     if (!ui.window)
     {
-        error(0, 0, "Could not create SDL_Window window: %s", SDL_GetError());
+        error(0, 0, "Could not create SDL_Window window: %s\n", SDL_GetError());
         return false;
     }
 
     ui.renderer = SDL_CreateRenderer(ui.window, -1, 0);
     if (!ui.renderer)
     {
-        error(0, 0, "Could not create SDL_renderer renderer: %s", SDL_GetError());
+        error(0, 0, "Could not create SDL_renderer renderer: %s\n", SDL_GetError());
         return false;
     }
 
     ui.video_buffer = SDL_CreateRGBSurfaceWithFormat(0, SCREEN_WIDTH, SCREEN_HEIGHT, 1, SDL_PIXELFORMAT_INDEX8);
     if (!ui.video_buffer)
     {
-        error(0, 0, "Could not create SDL_Surface video_buffer: %s", SDL_GetError());
+        error(0, 0, "Could not create SDL_Surface video_buffer: %s\n", SDL_GetError());
+        return false;
+    }
+
+    if (SDL_SetRenderDrawColor(ui.renderer, 0, 0, 0, SDL_ALPHA_OPAQUE) == -1)
+    {
+        error(0, 0, "Could not set Renderer draw color: %s\n", SDL_GetError());
         return false;
     }
 
